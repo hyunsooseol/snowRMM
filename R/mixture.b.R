6c84c5707a4a9514b36f5c226330333286ca6789
hyunsooseol <snow@cau.ac.kr>
2020-08-07 14:58:34 +0900

    Adding Mixture Model

---
+++ R/mixture.b.R
@@ -0,0 +1,26 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+#' Mixture Rasch Analysis
+#' @importFrom R6 R6Class
+#' @import jmvcore
+#' @importFrom mixRasch mixRasch
+#' @import mixRasch
+#' @import mixRaschTools
+#' @importFrom boot boot
+#' @import boot 
+#' @importFrom boot boot.ci
+#' @importFrom mixRaschTools mixRasch.plot
+#' @importFrom mixRaschTools avg.theta
+#' @export
+
+
+mixtureClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "mixtureClass",
+    inherit = mixtureBase,
+    private = list(
+        .run = function() {
+
+          
+        })
+)
--- R/rasch.b.R
+++ R/rasch.b.R
@@ -416,8 +416,74 @@ raschClass <- if (requireNamespace('jmvcore')) R6::R6Class(
          #compute wright---
          
          wright = mixRasch::mixRasch(data=data, steps=step, model=type, n.c=1)
-           
-           
+          
+         # personItemPlot---  
+         # personItemPlot <-
+         #   function(raschResult, nBreaks=15, plotTitle="Person Item Histogram",
+         #            xlab = "Relative Frequency", ylab = "Ability",
+         #            col = c("darkgrey","lightgrey"), colTheme, makeLegend=TRUE,
+         #            legendLabels=c("items", "people"), legendLoc="bottomleft",
+         #            gDevice, file){
+         # 
+         #     if(! missing(colTheme)){
+         #       if(colTheme=="dukes"){
+         #         col <- c("#450084","#CBB677")
+         #       } else if(colTheme=="spartans"){
+         #         col <- c("#003366","#FFCC00")
+         #       } else if(colTheme=="cavaliers"){
+         #         col <- c("#FF7003","#0D3268")
+         #       } else{
+         #         col <- c("darkgrey","lightgrey")
+         #       }
+         #     }
+         # 
+         #     measureRange <- function(items, people){
+         #       mRange <- range(c(items, people))
+         #       mRange[1] <- floor(mRange[1]*10)/10
+         #       mRange[2] <- ceiling(mRange[2]*10)/10
+         #       mRange
+         #     }
+         # 
+         #     mRange <- measureRange(raschResult$item.par$delta.i,raschResult$person.par$theta)
+         #     breakArray <- seq(mRange[1], mRange[2], (mRange[2]-mRange[1])/nBreaks)
+         # 
+         #     itemCounts <- hist(raschResult$item.par$delta.i,plot=FALSE,breaks=breakArray)
+         #     personCounts <- hist(raschResult$person.par$theta,plot=FALSE,breaks=breakArray)
+         # 
+         #     itemCounts$density <- itemCounts$density/sum(itemCounts$density)
+         #     personCounts$density <- personCounts$density/sum(personCounts$density)
+         # 
+         #     maxPeople <- max(personCounts$density)
+         #     maxItems <-  max(itemCounts$density)
+         # 
+         #     startScale <- personCounts$mids[1]
+         #     unitInc <- personCounts$mids[2] - personCounts$mids[1]
+         # 
+         # 
+         #     plot(1, type="n",xlim=c(-maxItems,maxPeople),ylim=c(0,nBreaks), main=plotTitle,
+         #          xlab=xlab, ylab=ylab, yaxt="n", xaxt="n")
+         # 
+         #     axYPoints <- floor(mRange[1]):floor(mRange[2])
+         #     axis(2,axYPoints,at=((1 + (axYPoints - startScale)/unitInc)))
+         # 
+         #     axXPoints <- seq(ceiling(maxItems*10)/10,0,by=-.1)
+         #     atXPoints <- seq(-ceiling(maxItems*10)/10,0,by=.1)
+         #     axis(1,axXPoints,at=atXPoints)
+         # 
+         #     axXPoints <- seq(.1, ceiling(maxPeople*10)/10,by=.1)
+         #     atXPoints <- seq(.1, ceiling(maxPeople*10)/10,by=.1)
+         #     axis(1,axXPoints,at=atXPoints)
+         # 
+         #     barplot(-itemCounts$density, col=col[1] , horiz=TRUE, space=0, add=TRUE, axes=FALSE)
+         #     barplot(personCounts$density, col=col[2], horiz=TRUE, space=0, add=TRUE, axes=FALSE)
+         # 
+         #     if(makeLegend){
+         #       legend(legendLoc, legendLabels, col=col, pch=15, cex=1.2)
+         #     }
+         # 
+         #   }
+         # 
+         # 
        # Prepare Data For wrightmap Plot -------
            
            image <- self$results$wrightmap
@@ -437,7 +503,8 @@ raschClass <- if (requireNamespace('jmvcore')) R6::R6Class(
            
            
            wright <- image$state
-           
+          
+          
    plot <- mixRasch::personItemPlot(wright)
                                    
                
@@ -462,9 +529,6 @@ raschClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             data <- jmvcore::naOmit(data)
             
             return(data)
-        }          
-            
+        }
 
-           
-        )
-)
+))
