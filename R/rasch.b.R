881d1ad3234bf59ec6dcf4544c0f52fbf36d4a37
hyunsooseol <snow@cau.ac.kr>
2022-09-12 13:48:42 +0900

    added instructions in Rasch model

--- R/rasch.b.R
+++ R/rasch.b.R
@@ -43,10 +43,11 @@ raschClass <- if (requireNamespace('jmvcore'))
             <p>_____________________________________________________________________________________________</p>
             <p>1. The standard Rasch model is performed by Jonint Maximum Liklihood(JML).</p>
             <p>2. Specify </b> the number of 'Step' and model 'Type'</b> in the 'Analysis option'.</p>
-            <P>3. Step is defined as number of <b>category-1</b>. </p>
-            <p>4. <b>Person Analysis</b> will be displayed in the datasheet.</p>
-            <p>5. The <b>eRm</b> R package was used for the person-item map for PCM.</p>
-            <p>6. Feature requests and bug reports can be made on my <a href='https://github.com/hyunsooseol/snowRMM/issues'  target = '_blank'>GitHub</a>.</p>
+            <p>3. Step is defined as number of <b>category-1</b>. </p>
+            <p>4. The bottom category should be coded as 0.</p>
+            <p>5. <b>Person Analysis</b> will be displayed in the datasheet.</p>
+            <p>6. The <b>eRm</b> R package was used for the person-item map for PCM.</p>
+            <p>7. Feature requests and bug reports can be made on my <a href='https://github.com/hyunsooseol/snowRMM/issues'  target = '_blank'>GitHub</a>.</p>
             <p>_____________________________________________________________________________________________</p>
             
             </div>
@@ -169,7 +170,7 @@ raschClass <- if (requireNamespace('jmvcore'))
         
         ###################
         
-        if(self$options$rsm==TRUE | self$options$pcm==TRUE){
+      #  if(self$options$rsm==TRUE | self$options$pcm==TRUE){
           
           rsm.res <- eRm::RSM(data)
           tab<- thresholds(rsm.res)
@@ -192,7 +193,9 @@ raschClass <- if (requireNamespace('jmvcore'))
           
           #####################################################
           
-          table <- self$results$rsm
+          if(self$options$rsm==TRUE){ 
+          
+           table <- self$results$rsm
           
           nCategory <- nc
           
@@ -223,14 +226,15 @@ raschClass <- if (requireNamespace('jmvcore'))
               
             }
             
-            #   row[["rm"]] <- rm[i]
+         
             
             table$setRow(rowNo = i, values = row)
           }
           
-          
+          } 
           ######################################
-          
+         
+          if(self$options$pcm==TRUE){  
           
           table <- self$results$pcm
           
@@ -264,14 +268,12 @@ raschClass <- if (requireNamespace('jmvcore'))
               
             }
             
-            #  row[["pm"]] <- pm[i]
-            
+          
             table$setRow(rowNo = i, values = row)
           }
           
+          }
           
-        }
-        
         
         # compute results------
         
