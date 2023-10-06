7158b7243a9f63fce3f4f055556645312bf03d9c
hyunsooseol <snow@cau.ac.kr>
2023-10-06 14:25:52 +0900

    check ref.

--- R/00jmv.R
+++ R/00jmv.R
@@ -7,7 +7,7 @@
         `author`="Seol, H.", 
         `year`=2023, 
         `title`="snowRMM: Rasch Mixture, LCA, and Test Equating Analysis", 
-        `publisher`="(Version 5.6.8)[jamovi module]. URL https://github.com/hyunsooseol/snowRMM", 
+        `publisher`="(Version 5.6.8) [jamovi module]. URL https://github.com/hyunsooseol/snowRMM", 
         `url`="https://github.com/hyunsooseol/snowRMM"), 
     `mixRasch`=list(
         `type`="software", 
