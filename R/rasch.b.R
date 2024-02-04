b66ee7fd210d31aa9bcf3799a8b46dabfd02f8ce
hyunsooseol <snow@cau.ac.kr>
2024-02-04 17:46:14 +0900

    fixed UI 2024

--- R/rasch.b.R
+++ R/rasch.b.R
@@ -50,14 +50,13 @@ raschClass <- if (requireNamespace('jmvcore'))
             <div class='instructions'>
             
             <p>_____________________________________________________________________________________________</p>
-            <p>1. The <b>MixRasch</b> R package was used for the Rasch model using joint maximum likelihood estimation(JMLE).</P>
+            <p>1. The <b>mixRasch</b> R package was used for the Rasch model using joint maximum likelihood estimation(JMLE).</P>
             <p>2. Specify </b> the number of <b>Step</b> and model <b>Type</b> in the analysis option.</p>
             <p>3. Step is defined as number of <b>category-1</b>. </p>
             <p>4. The minimum and maximum values of a category must be the same across all items for <b>rating sclaes</b> with <b>eRm</b> R package.</p>
             <p>5. <b>Person Analysis</b> will be displayed in the datasheet.</p>
             <p>6. The <b>eRm</b> R package was used for the person-item map for PCM.</p>
-            <p>7. The <b>pairwise</b> R package was used for residual analysis.</p>
-            <p>8. Feature requests and bug reports can be made on my <a href='https://github.com/hyunsooseol/snowRMM/issues'  target = '_blank'>GitHub</a>.</p>
+            <p>7. Feature requests and bug reports can be made on my <a href='https://github.com/hyunsooseol/snowRMM/issues'  target = '_blank'>GitHub</a>.</p>
             <p>_____________________________________________________________________________________________</p>
             
             </div>
